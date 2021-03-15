37a5162efc1662092713fea1a8868a23e6b775ae
smenon1111 <42555379+smenon1111@users.noreply.github.com>
2021-03-15 15:28:49 -0700

    Merge branch 'master' of C:\Users\smeno\AndroidStudioProjects\InformationApp2021 with conflicts.

--- app/src/main/java/com/example/informationapp2021/DialogFlow.java
+++ app/src/main/java/com/example/informationapp2021/DialogFlow.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import androidx.fragment.app.Fragment;
 
 public class DialogFlow extends Fragment {
@@ -16,18 +15,16 @@ public class DialogFlow extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View dialogFlowView = inflater.inflate(R.layout.activity_main, container, false);
+        View dialogFlowView = inflater.inflate(R.layout.dialogflow, container, false);
 
 
-        DFBtn= dialogFlowView.findViewById(R.id.button);
+        DFBtn = dialogFlowView.findViewById(R.id.button);
 
         DFBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("https://bot.dialogflow.com/cf5690aa-1e88-4fc8-a285-3747b968397f"));
+                //TODO: Put into fragment form instead of intent
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://bot.dialogflow.com/cf5690aa-1e88-4fc8-a285-3747b968397f"));
                 startActivity(intent);
             }
         });
